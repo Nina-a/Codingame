5a25f3b54a8a775e445ed78d05d8a59f234c658c
Nina-a <nina.leduc@gmail.com>
2021-04-20 16:12:00 +0200

    pass test 2

--- moyen/the_last_crusade_ep1.ts
+++ moyen/the_last_crusade_ep1.ts
@@ -7,19 +7,13 @@
  const W: number = parseInt(inputs[0]); // number of columns.
  const H: number = parseInt(inputs[1]); // number of rows.
  let type: string;
- let parcours = new Array<Array<number>>();
+ let parcours = new Array<Array<string>>();
  for (let i = 0; i < H; i++) {
      var LINE: string = readline(); // represents a line in the grid and contains W integers. Each integer represents one room of a given type.
      console.error("line " + LINE);
-     parcours[i] = [];
-     for (let j = 0 ; j < LINE.length; j++){
- 
-         type = LINE.charAt(j);
-         
-         if (type != " ") {
-             parcours[i].push(parseInt(type));
-         }
-     }
+     let plop = LINE.split(" ");
+     console.error("plop "+plop);
+     parcours.push(plop);
  }
  
  const EX: number = parseInt(readline()); // the coordinate along the X axis of the exit (not useful for this first mission, but must be read).
@@ -30,17 +24,35 @@
      const XI: number = parseInt(inputs[0]);
      const YI: number = parseInt(inputs[1]);
      const POS: string = inputs[2];
-     let type_case = parcours[YI][XI];
+     let type_case = parseInt(parcours[YI][XI]);
      console.error("type_case " + type_case);
      console.error(XI,YI,POS);
      switch(type_case){
-         case 3:
+        case 3:
              var X = XI;
              var Y = YI +1;
          break;
          case 11:
              var X = XI + 1;
              var Y = YI;
+         break;
+         case 2:
+             if (POS === "LEFT"){
+                 var X = XI + 1;
+                 var Y = YI; 
+             }else{
+                 var X =XI - 1;
+                 var Y = YI;
+             }
+         break;
+         case 10:
+             var X = XI - 1;
+             var Y= YI;
+         break;
+         case 13:
+             var X = XI;
+             var Y = YI + 1;
+         break;
      }
  
      // One line containing the X Y coordinates of the room in which you believe Indy will be on the next turn.
